d52b02616971d5e7b44abe462bec517c3905e2f4
happyeggchen <tsingkwai@protonmail.com>
2022-05-27 14:47:19 +0800

    env get fixed

--- codes/run/chroot/chroot_run.fish
+++ codes/run/chroot/chroot_run.fish
@@ -19,13 +19,13 @@ function chroot_run
     essential_mount
     switch $ctcontainer_safety_level
         case -1
-            sudo unshare -impuCTf chroot --userspec safety:safety $container env -C /home/safety XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR HOME=/home/safety USER=safety $argv[2..-1]
+            sudo unshare -impuCTf chroot --userspec safety:safety $container XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR HOME=/home/safety USER=safety $argv[2..-1]
         case 0
-            sudo unshare -impuCTf chroot $container env -C /root DISPLAY=:0 XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR $argv[2..-1]
+            sudo unshare -impuCTf chroot $container env DISPLAY=:0 XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR $argv[2..-1]
         case 1
-            sudo unshare -impuCTf chroot $container env -C /root DISPLAY=:0 $argv[2..-1]
+            sudo unshare -impuCTf chroot $container env DISPLAY=:0 $argv[2..-1]
         case 2
-            sudo unshare -impuCTf chroot --userspec safety:safety $container env -C /home/safety HOME=/home/safety USER=safety $argv[2..-1]
+            sudo unshare -impuCTf chroot --userspec safety:safety $container env HOME=/home/safety USER=safety $argv[2..-1]
         case h '*'
             logger 4 "can't understand what is safety_level.ctcontainer{$ctcontainer_safety_level},abort"
             exit
