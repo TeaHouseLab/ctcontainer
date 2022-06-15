38e020c078aa9a2b6507c4eb2e93af861daba58d
tsingkwai <tsingkwai@protonmail.com>
2022-06-15 21:33:31 +0800

    X11 is enabled now by default

--- codes/run/chroot/chroot_run.fish
+++ codes/run/chroot/chroot_run.fish
@@ -19,13 +19,13 @@ function chroot_run
     essential_mount
     switch $ctcontainer_safety_level
         case -1
-            sudo unshare -impuCTf chroot --userspec safety:safety $container env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR HOME=/home/safety USER=safety $argv[2..-1]
+            sudo unshare -impuCTf chroot --userspec safety:safety $container env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR DISPLAY=:0 HOME=/home/safety USER=safety $argv[2..-1]
         case 0
             sudo unshare -impuCTf chroot $container env DISPLAY=:0 XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR $argv[2..-1]
         case 1
             sudo unshare -impuCTf chroot $container env DISPLAY=:0 $argv[2..-1]
         case 2
-            sudo unshare -impuCTf chroot --userspec safety:safety $container env HOME=/home/safety USER=safety $argv[2..-1]
+            sudo unshare -impuCTf chroot --userspec safety:safety $container env DISPLAY=:0 HOME=/home/safety USER=safety $argv[2..-1]
         case h '*'
             logger 4 "can't understand what is safety_level.ctcontainer{$ctcontainer_safety_level},abort"
             exit
--- codes/run/nspawn/nspawn_run.fish
+++ codes/run/nspawn/nspawn_run.fish
@@ -15,16 +15,17 @@ function nspawn_run
         exit
     end
     setup_dir_nspawn
+    setup_user_xorg
     cd $ctcontainer_root
     switch $ctcontainer_safety_level
         case -1
-            sudo systemd-nspawn --resolv-conf=off -q -u safety -D $container env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR HOME=/home/safety USER=safety $argv[2..-1]
+            sudo systemd-nspawn --resolv-conf=off -q -u safety -D $container env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR DISPLAY=:0 HOME=/home/safety USER=safety $argv[2..-1]
         case 0
             sudo systemd-nspawn --resolv-conf=off -b -q -D $container
         case 1
             sudo systemd-nspawn --resolv-conf=off -q -D $container env DISPLAY=:0 $argv[2..-1]
         case 2
-            sudo systemd-nspawn --resolv-conf=off -q -u safety -D $container env HOME=/home/safety USER=safety $argv[2..-1]
+            sudo systemd-nspawn --resolv-conf=off -q -u safety -D $container env DISPLAY=:0 HOME=/home/safety USER=safety $argv[2..-1]
         case h '*'
             logger 4 "can't understand what is $ctcontainer_safety_level,abort"
             exit
