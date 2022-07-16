10fecacd84e41b162552ee1e1e4120526d49fd9d
tsingkwai <tsingkwai@protonmail.com>
2022-07-16 17:15:00 +0800

    Avoid remove whole system when purging a running container

--- codes/misc/purge.fish
+++ codes/misc/purge.fish
@@ -1,7 +1,20 @@
 function purge
+    set -lx container
     for container in $argv[1..-1]
         container_tester $container
         cd $ctcontainer_root
+        if grep -qs "$ctcontainer_root/$container" /proc/mounts
+            logger 3 "A container is running, Umount and purge it?[y/n]"
+            read -n1 -P "$prefix >>> " _purge_
+            switch $_purge_
+                case y Y
+                    set ctcontainer_auto_umount 1
+                    essential_umount
+                case n N '*'
+                    logger 1 Abort
+                    exit
+            end
+        end
         if test -e /etc/systemd/system/ctcontainer-$container.service
             service remove $container
         end
